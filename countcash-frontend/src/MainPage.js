854425dc55c46d95d2897695daf78ba5fb1a84db
SEBASTIAN SOTO MATOS <1087765@est.intec.edu.do>
2021-10-20 20:50:10 -0400

    Reverted Main Page

--- countcash-frontend/src/MainPage.js
+++ countcash-frontend/src/MainPage.js
@@ -1,29 +1,14 @@
 import React, {Component} from 'react';
 import {Button} from 'react-bootstrap';
-import { AddFlowModal } from './AddFlowModal';
 import './styles.css';
 
 export class MainPage extends Component{
 
-    constructor(props){
-        super(props);
-        this.state={flows:[], addModalShow:false}
-    }
-
     render(){
-        const {flows}=this.state;
-        let addModalClose=()=>this.setState({addModalShow:false});
         return(
             <div className="text-center p-3">
-            <Button variant = "success btn-agregar"
-            onClick={()=>this.setState({addModalShow:true})}>
-            Agregar</Button>
-            
-            <AddFlowModal show={this.state.addModalShow}
-            onHide={addModalClose}/>
-            
-            <Button variant = "danger btn-eliminar  ">
-            Eliminar</Button>
+            <Button variant = "success btn-agregar">Agregar</Button>
+            <Button variant = "danger btn-eliminar">Eliminar</Button>
             </div>
         );                
     }
