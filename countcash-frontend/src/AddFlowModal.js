a62bf569e49aca293f3d6886c3f0f425c9969234
SEBASTIAN SOTO MATOS <1087765@est.intec.edu.do>
2021-10-24 11:34:47 -0400

    Modal changes pt 4

--- countcash-frontend/src/AddFlowModal.js
+++ countcash-frontend/src/AddFlowModal.js
@@ -3,9 +3,10 @@ import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
 
 const baseUrl = "https://localhost:5001/api/FlujoUsuario/PostFlujoUsuario";
 export class AddFlowModal extends Component{
+    
     constructor(props){
         super(props);
-        this.state = { flows: [] };
+        this.state = { flow: [] };
         this.handleSubmit=this.handleSubmit.bind(this);
     }
     
@@ -13,17 +14,17 @@ export class AddFlowModal extends Component{
         fetch(baseUrl)
             .then(response => response.json())
             .then(data => {
-                this.setState({ flows: data })
+                this.setState({ flow: data })
             });
     }
 
-    // componentDidMount() {
-    //     this.refreshList();
-    // }
+    componentDidMount() {
+        this.refreshList();
+    }
 
-    // componentDidUpdate() {
-    //     this.refreshList();
-    // }
+    componentDidUpdate() {
+        this.refreshList();
+    }
 
     onChange = e =>{
         this.setState({value : e.target.value});
@@ -37,13 +38,13 @@ export class AddFlowModal extends Component{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify({
+            body:{
                 TipoFlujo: event.target.TipoFlujo.value,
                 Monto: event.target.Monto.value,
                 Descripcion: event.target.Descripcion.value,
                 // Flujo: event.target.Flujo.value,
                 TipoMonto: event.target.TipoMonto.value
-            })
+            }
         })
         .then(res=>res.json())
         .then((result)=>{
@@ -59,7 +60,7 @@ export class AddFlowModal extends Component{
         return(
             <div className="container">
                 <Modal {...this.props} aria-labelledby="contained-modal-title-vcenter" centered>
-                    <Modal.Header clooseButton>
+                    <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title-vcenter">
                             Agregar flujo
                         </Modal.Title>
@@ -70,13 +71,13 @@ export class AddFlowModal extends Component{
                                 <Form.Check
                                     inline
                                     label="Gasto"
-                                    name="tipoFlujo"
+                                    name="TipoFlujo"
                                     type="radio"/>
 
                                 <Form.Check
                                     inline
                                     label="Ingreso"
-                                    name="tipoFlujo"
+                                    name="TipoFlujo"
                                     type="radio"/>
                             </Form.Group>
 
@@ -85,7 +86,7 @@ export class AddFlowModal extends Component{
                                     Monto:
                                 </Form.Label>
                                 <Col>
-                                    <Form.Control type="text" required placeholder="0.00"/>
+                                    <Form.Control name= "Monto" type="text" required placeholder="0.00"/>
                                 </Col>
                             </Form.Group>
                                         
@@ -97,7 +98,7 @@ export class AddFlowModal extends Component{
                                 <Form.Label>
                                     Tipo de Monto
                                 </Form.Label>
-                                <Form.Select defaultValue="Choose...">
+                                <Form.Select name="TipoMonto" defaultValue="Choose...">
                                     <option>Ropa</option>
                                     <option>Recreacion</option>
                                     <option>Educacion</option>
@@ -110,7 +111,7 @@ export class AddFlowModal extends Component{
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="primary" type="submit">
+                        <Button variant="success" type="submit">
                             Agregar
                         </Button>
                         <Button variant="danger" onClick={this.props.onHide}>
