92be6d6059e650862edfcab2c1b129865b7bd681
SEBASTIAN SOTO MATOS <1087765@est.intec.edu.do>
2021-10-24 13:15:18 -0400

    Modal changes pt 5

--- countcash-frontend/src/AddFlowModal.js
+++ countcash-frontend/src/AddFlowModal.js
@@ -1,37 +1,45 @@
 import React,{Component} from "react";
 import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
-
+import Cookies from 'universal-cookie';
+var cookies = new Cookies();
 const baseUrl = "https://localhost:5001/api/FlujoUsuario/PostFlujoUsuario";
 export class AddFlowModal extends Component{
     
     constructor(props){
         super(props);
-        this.state = { flow: [] };
+        // this.state = { flow: [] };
         this.handleSubmit=this.handleSubmit.bind(this);
     }
     
-    refreshList() {
-        fetch(baseUrl)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ flow: data })
-            });
-    }
+    // refreshList() {
+    //     fetch(baseUrl)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             this.setState({ flow: data })
+    //         });
+    // }
 
-    componentDidMount() {
-        this.refreshList();
-    }
+    // componentDidMount() {
+    //     this.refreshList();
+    // }
 
-    componentDidUpdate() {
-        this.refreshList();
-    }
+    // componentDidUpdate() {
+    //     this.refreshList();
+    // }
 
-    onChange = e =>{
-        this.setState({value : e.target.value});
-    }
+    // onChange = e =>{
+    //     this.setState({value : e.target.value});
+    // }
+
+    // handleChange = e =>{
+    //     const { name, value } = e.target;
+    // }
 
     handleSubmit(event){
         event.preventDefault();
+        const data={
+            usuarioID: cookies.get("id")
+        }
         fetch(baseUrl,{
             method:'POST',
             header:{
@@ -39,16 +47,18 @@ export class AddFlowModal extends Component{
                 'Content-Type':'application/json'
             },
             body:{
-                TipoFlujo: event.target.TipoFlujo.value,
-                Monto: event.target.Monto.value,
-                Descripcion: event.target.Descripcion.value,
+                usuarioID: data.usuarioID,
+                tipoFlujoID: event.target.TipoFlujoID.value,
+                monto: event.target.Monto.value,
+                descripcion: event.target.Descripcion.value,
                 // Flujo: event.target.Flujo.value,
-                TipoMonto: event.target.TipoMonto.value
+                tipoMontoID: event.target.TipoMontoID.value
             }
         })
         .then(res=>res.json())
         .then((result)=>{
             alert(result);
+            console.log(event.target.tipoFlujoID.value)
         },
         (error)=>{
             alert('No se pudo agregar el flujo.');
@@ -56,7 +66,7 @@ export class AddFlowModal extends Component{
     }
 
     render(){
-        const { flow } = this.state;
+        // const { flow } = this.state;
         return(
             <div className="container">
                 <Modal {...this.props} aria-labelledby="contained-modal-title-vcenter" centered>
@@ -67,18 +77,20 @@ export class AddFlowModal extends Component{
                     </Modal.Header>
                     <Modal.Body>
                         <Form onSubmit={this.handleSubmit}>
-                            <Form.Group className="mb-3" controlId="TipoFlujo">    
+                            <Form.Group className="mb-3" controlId="TipoFlujoID">    
                                 <Form.Check
                                     inline
                                     label="Gasto"
                                     name="TipoFlujo"
-                                    type="radio"/>
+                                    type="radio"
+                                    value="2"/>
 
                                 <Form.Check
                                     inline
                                     label="Ingreso"
                                     name="TipoFlujo"
-                                    type="radio"/>
+                                    type="radio"
+                                    value="1"/>
                             </Form.Group>
 
                             <Form.Group as={Row} className="mb-3" controlId="Monto">
@@ -86,7 +98,7 @@ export class AddFlowModal extends Component{
                                     Monto:
                                 </Form.Label>
                                 <Col>
-                                    <Form.Control name= "Monto" type="text" required placeholder="0.00"/>
+                                    <Form.Control name= "Monto" type="number" required placeholder="0.00"/>
                                 </Col>
                             </Form.Group>
                                         
@@ -98,22 +110,26 @@ export class AddFlowModal extends Component{
                                 <Form.Label>
                                     Tipo de Monto
                                 </Form.Label>
-                                <Form.Select name="TipoMonto" defaultValue="Choose...">
-                                    <option>Ropa</option>
-                                    <option>Recreacion</option>
-                                    <option>Educacion</option>
-                                    <option>Vivienda</option>
-                                    <option>Comida</option>
-                                    <option>Transporte y Comunicacion</option>
-                                    <option>Ingreso</option>
+                                <Form.Select name="TipoMontoID" defaultValue="Choose...">
+                                    <option> Choose...</option>
+                                    <option value="1">Ropa</option>
+                                    <option value="2">Recreacion</option>
+                                    <option value="3"> Educacion</option>
+                                    <option value="4">Vivienda</option>
+                                    <option value="5">Comida</option>
+                                    <option value="6">Transporte y Comunicacion</option>
+                                    <option value="7">Ingreso</option>
                                 </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Button variant="success" type="submit">
+                                    Agregar
+                                </Button>
                             </Form.Group>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="success" type="submit">
-                            Agregar
-                        </Button>
                         <Button variant="danger" onClick={this.props.onHide}>
                             Cancelar
                         </Button>
